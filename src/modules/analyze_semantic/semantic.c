9fa8b6192f5add8ba8c82f9bda93f3a3276ed461
kinakqna <tomokirenrakuyo@gmail.com>
2025-04-16 19:16:16 +0900

    feat(semantic): Implement basic framework for semantic analysis

--- src/modules/analyze_semantic/semantic.c
+++ src/modules/analyze_semantic/semantic.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   semantic.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/11 16:40:50 by teando            #+#    #+#             */
-/*   Updated: 2025/04/16 11:08:28 by teando           ###   ########.fr       */
+/*   Created: Invalid date        by                   #+#    #+#             */
+/*   Updated: 2025/04/16 19:10:32 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mod_sem.h"
 
-//wordを引数にコネコネする関数→ft_lstiterみたいなやつで全部に適用したら良さそうかも
+// wordを引数にコネコネする関数→ft_lstiterみたいなやつで全部に適用したら良さそうかも
 // 文字リテラル
 // → リダイレクト
 // → CMD 解決
@@ -26,44 +26,85 @@ int ft_isbackslush(int c)
 	return (c == '\\');
 }
 
-int proc_argv(t_lexical_token *data, int count, t_shell *shell)
+char	*handle_wildcard(char *in, t_shell *shell)
 {
-	// 文字リテラル
+	char *buf;
 
-	// CMD 解決
+	return buf;
+}
+
+char	*handle_env(char *in, t_shell *shell)
+{
+	char *buf;
+
+	return buf;
+}
+
+int	resolve_path(char *in, t_shell *shell)
+{
 	return (0);
 }
 
-int proc_redr(t_lexical_token *data, int count, t_shell *shell)
+int proc_argv(t_lexical_token *data, int count, t_shell *shell)
 {
 	// 文字リテラル
+	data->value = handle_wildcard(handle_env(data->value,shell),shell);
+	if (!data->value)
+		return (1);
+	// CMD 解決
+	if (count == 0)
+		if (resolve_path(data->value, shell))
+			rteurn (1);
+	return (0);
+}
 
+int	valid_redir(t_lexical_token *data, t_shell *shell)
+{
+	return (0);
+}
+
+int	proc_redr(t_lexical_token *data, int count, t_shell *shell)
+{
+	// 文字リテラル
+	data->value = handle_wildcard(handle_env(data->value,shell),shell);
+	if (!data->value)
+		return (1);
 	// リダイレクト
+	valid_redir(data, shell);
 	return (0);
 }
 
 int	ast2cmds(t_ast *ast, t_shell *shell)
 {
+	int	status;
+
+	status = 0;
 	if (ast == NULL)
 		return (1);
 	if (ast->ntype != NT_CMD)
 	{
-		ast2cmds(ast->left, shell);
-		ast2cmds(ast->right, shell);
+		status += ast2cmds(ast->left, shell);
+		status += ast2cmds(ast->right, shell);
 	}
 	else
 	{
-		ms_lstiter(ast->args->argv, proc_argv, shell);// NULLは適用するコマンドを入れるところまだ未実装
-		ms_lstiter(ast->args->redr, proc_redr, shell);// NULLは適用するコマンドを入れるところまだ未実装
+		if (ms_lstiter(ast->args->argv, proc_argv, shell))
+			return (1);
+		if (ms_lstiter(ast->args->redr, proc_redr, shell))
+			return (1);
 	}
-	return (0);
+	return ((0 < status));
 }
 
 t_status	mod_sem(t_shell *shell)
 {
-	t_ast *ast;
+	t_ast	*ast;
 
 	ast = shell->ast;
-
+	if (ast2cmds(ast, shell))
+	{
+		//エラーハンドリング
+		return (shell->status);
+	}
 	return (E_NONE);
 }
