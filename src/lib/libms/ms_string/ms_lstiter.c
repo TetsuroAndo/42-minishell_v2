07cfb0daba8d47ac8ab602d899a4c3273c2d9d6b
kinakqna <tomokirenrakuyo@gmail.com>
2025-04-16 21:15:09 +0900

    refactor(ms_lstiter): allow function to modify list elements

--- src/lib/libms/ms_string/ms_lstiter.c
+++ src/lib/libms/ms_string/ms_lstiter.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ms_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 18:49:43 by tomsato           #+#    #+#             */
-/*   Updated: 2025/04/16 11:05:21 by teando           ###   ########.fr       */
+/*   Updated: 2025/04/16 21:00:51 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libms.h"
 #include <stddef.h>
 
-int	ms_lstiter(t_list *lst, int (*f)(void *, int, t_shell *), t_shell *shell)
+int	ms_lstiter(t_list *lst, int (*f)(t_list **, void *, int, t_shell *), t_shell *shell)
 {
 	int	i;
 
@@ -22,7 +22,7 @@ int	ms_lstiter(t_list *lst, int (*f)(void *, int, t_shell *), t_shell *shell)
 		return (1);
 	while (lst != NULL)
 	{
-		if (f(lst->data, i, shell))
+		if (f(&lst , lst->data, i, shell))
 			return (1);
 		lst = lst->next;
 		i++;
