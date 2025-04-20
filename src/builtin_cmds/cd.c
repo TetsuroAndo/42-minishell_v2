7e77f014dcdcb408d7f1f07af9abb2ac4fb6c52c
tomsato42 <tomokirenrakuyo@gmail.com>
2025-04-21 01:25:52 +0900

    fix: cd

--- src/builtin_cmds/cd.c
+++ src/builtin_cmds/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: tomsato <tomsato@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 16:59:45 by teando            #+#    #+#             */
-/*   Updated: 2025/04/19 03:11:52 by teando           ###   ########.fr       */
+/*   Updated: 2025/04/21 01:24:58 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ t_status	__cd_tilde(char path[], char *arg, t_shell *sh)
 t_status	__cd_oldpwd(char path[], char *arg, t_shell *sh)
 {
 	char	*oldpwd;
-	
+
 	(void)arg;
 	oldpwd = ms_getenv("OLDPWD", sh);
 	if (oldpwd[0] == '\0')
@@ -54,8 +54,10 @@ t_status	__cd_oldpwd(char path[], char *arg, t_shell *sh)
 t_status	__cd(int argc, char **argv, t_shell *sh)
 {
 	char	mv_dir[PATH_MAX + 1];
-	
-	if (argc == 1 || argv[1][0] == '~')
+
+	if (argc >= 3)
+		return (ft_dprintf(2, "minishell: cd: too many arguments\n"), 1);
+	else if (argc == 1 || (argv[1][0] == '~' && argv[1][1] == '\0'))
 	{
 		if (__cd_tilde(mv_dir, argv[1], sh) == -1)
 			return (ft_dprintf(2, "cd: %s: %s\n", mv_dir, strerror(errno)), 1);
@@ -65,10 +67,6 @@ t_status	__cd(int argc, char **argv, t_shell *sh)
 		if (__cd_oldpwd(mv_dir, argv[1], sh) == -1)
 			return (ft_dprintf(2, "cd: %s: %s\n", mv_dir, strerror(errno)), 1);
 	}
-	else if (argc == 3)
-		return (ft_dprintf(2, "cd: string not in pwd: %s\n", argv[1]), 1);
-	else if (argc > 3)
-		return (ft_dprintf(2, "minishell: cd: too many arguments\n"), 1);
 	else
 	{
 		if (path_launcher(mv_dir, argv[1], F_OK, sh) == -1)
