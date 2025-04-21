b0b02e4b8fa129b3ccda8ed03b09b74791a96228
Tetsuro Ando <tetsuro997@gmail.com>
2025-04-21 22:33:06 +0900

    fix: echo option req

--- src/builtin_cmds/echo.c
+++ src/builtin_cmds/echo.c
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 01:23:44 by teando            #+#    #+#             */
-/*   Updated: 2025/04/21 15:48:52 by teando           ###   ########.fr       */
+/*   Updated: 2025/04/21 22:20:39 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,24 @@ t_status	__echo(int argc, char **argv, t_shell *sh)
 {
 	int	i;
 	int	newline;
-	int op_end_flag;
+	int	op_end_flag;
 
 	(void)sh;
-	i = 1;
+	i = 0;
 	newline = 1;
-	op_end_flag = 0;		
-	while (i < argc)
+	op_end_flag = 0;
+	while (++i < argc)
 	{
-		if (!ft_strncmp(argv[i], "-n", 2) && !op_end_flag)
+		if (!op_end_flag && argv[i][0] == '-' && ft_strspn(argv[i] + 1,
+				"n") == ft_strlen(argv[i] + 1) && argv[i][1])
 		{
 			newline = 0;
-			++i;
 			continue ;
 		}
 		op_end_flag = 1;
 		ft_putstr_fd(argv[i], STDOUT_FILENO);
 		if (i + 1 < argc)
 			ft_putchar_fd(' ', STDOUT_FILENO);
-		++i;
 	}
 	if (newline)
 		ft_putchar_fd('\n', STDOUT_FILENO);
