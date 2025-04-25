5f836108ddfc85845baeb3cb9af7fe8bf962cffb
Tetsuro Ando <tetsuro997@gmail.com>
2025-04-25 12:51:42 +0900

    pr

--- src/core/debug/fin_sh.c
+++ src/core/debug/fin_sh.c
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   fin_sh.c                                           :+:      :+:    :+:   */
@@ -6,9 +6,9 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 16:23:34 by teando            #+#    #+#             */
-/*   Updated: 2025/04/25 10:50:38 by teando           ###   ########.fr       */
+/*   Updated: 2025/04/25 12:47:46 by teando           ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "core.h"
 
@@ -17,7 +17,5 @@ void	put_sh_final(t_shell *sh)
 	if (!sh)
 		return ;
 	ft_dprintf(STDERR_FILENO, "\n");
-	ft_dprintf(STDERR_FILENO, "[GC LINE NODE]: %d\n", ft_lstsize(sh->gcli));
-	ft_dprintf(STDERR_FILENO, "[GC LINE FREE]: %d\n",
-		ft_gc_free_info(sh->gcli));
+	ft_dprintf(STDERR_FILENO, "[GC LINE NODE]: %d | [GC LINE FREE]: %d\n", ft_lstsize(sh->gcli), ft_gc_free_info(sh->gcli));
 }
