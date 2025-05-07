99eb9cd3da22f50564478701e105f5b7bdb3eb7e
kinakqna <tomokirenrakuyo@gmail.com>
2025-05-07 17:17:19 +0900

    Add parentheses counting function to validate input syntax

--- src/modules/analyze_lexical/tokenizer.c
+++ src/modules/analyze_lexical/tokenizer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomsato <tomsato@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 14:06:12 by teando            #+#    #+#             */
-/*   Updated: 2025/04/27 22:11:26 by tomsato          ###   ########.fr       */
+/*   Updated: 2025/05/07 17:12:38 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,28 @@ static int	validate_input(t_shell *sh)
 	return (1);
 }
 
+static int	count_paren(t_shell *sh)
+{
+	t_list			*tokens;
+	t_lexical_token	*token;
+	size_t			l;
+	size_t			r;
+
+	l = 0;
+	r = 0;
+	tokens = sh->token_list;
+	while (tokens->next)
+	{
+		token = (t_lexical_token *)tokens->data;
+		if (token->type == TT_LPAREN)
+			l++;
+		if (token->type == TT_RPAREN)
+			r++;
+		tokens = tokens->next;
+	}
+	return (l == r);
+}
+
 t_status	mod_lex(t_shell *sh)
 {
 	sh->token_list = NULL;
@@ -48,6 +70,11 @@ t_status	mod_lex(t_shell *sh)
 		return (sh->status = E_SYNTAX);
 	if (!tokenize_line(sh))
 		return (sh->status = E_SYNTAX);
+	if (!count_paren(sh))
+	{
+		ft_dprintf(2, ES_TOKEN);
+		return (sh->status = E_SYNTAX);
+	}
 	if (sh->debug & DEBUG_LEX)
 		debug_print_token_list(sh->token_list);
 	return (E_NONE);
