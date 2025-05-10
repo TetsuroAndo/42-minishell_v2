e0ec9f242d38eb23539893e32f7f818e3433ba82
tomsato42 <tomokirenrakuyo@gmail.com>
2025-05-10 10:39:44 +0900

    fix ambiguous redirect error

--- src/modules/analyze_semantic/proc_redr_errs.c
+++ src/modules/analyze_semantic/proc_redr_errs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   proc_redr_errs.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
+/*   By: tomsato <tomsato@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 20:33:56 by tomsato           #+#    #+#             */
-/*   Updated: 2025/05/07 16:57:07 by tomsato          ###   ########.fr       */
+/*   Updated: 2025/05/10 10:30:09 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int	proc_redr_errs(t_lexical_token *data, t_shell *shell)
 {
 	if (!data->value)
 		return (E_SYSTEM);
-	if (*data->value == '\0' && data->type != TT_HEREDOC)
+	if (data->type != TT_HEREDOC && (*data->value == '\0' || ft_strchr(data->value, ' ')))
 		return (ft_dprintf(2, "minishell: ambiguous redirect\n"),
 			E_AMBIGUOUS_REDIR);
 	if (!ft_strchr(data->value, '\n'))
