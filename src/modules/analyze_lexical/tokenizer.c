25e110c5d524f558dca9716b3282c1a56807cbf8
tomsato42 <tomokirenrakuyo@gmail.com>
2025-05-10 10:40:03 +0900

    add count_paren

--- src/modules/analyze_lexical/tokenizer.c
+++ src/modules/analyze_lexical/tokenizer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
+/*   By: tomsato <tomsato@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 14:06:12 by teando            #+#    #+#             */
-/*   Updated: 2025/05/07 17:29:41 by tomsato          ###   ########.fr       */
+/*   Updated: 2025/05/10 10:09:58 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,28 @@ static int	validate_input(t_shell *sh)
 //     }
 //     return (0);
 // }
+
+static int	count_paren(t_shell *sh)
+{
+	t_list			*tokens;
+	t_lexical_token	*token;
+	size_t			l;
+	size_t			r;
+
+	l = 0;
+	r = 0;
+	tokens = sh->token_list;
+	while (tokens->next)
+	{
+		token = (t_lexical_token *)tokens->data;
+		if (token->type == TT_LPAREN)
+			l++;
+		if (token->type == TT_RPAREN)
+			r++;
+		tokens = tokens->next;
+	}
+	return (l == r);
+}
 
 /**
  * @brief 入力されたソースラインをトークン化し、その構文を検証する
